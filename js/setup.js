f84f397c9e64b7d3f7e6b4adca8521231277573a
natapd <nata_pd@mail.ru>
2019-01-31 13:17:57 +0400

    После 6ого задания

--- js/setup.js
+++ js/setup.js
@@ -24,7 +24,7 @@ var fireBallColors=['#ee4830','#30a8ee','#5ce6c0','#e848d5','#e6e848'];
 var CoatInput=Setup.querySelector('input[name=coat-color]');
 var EyesInput=Setup.querySelector('input[name=eyes-color]');
 var FireballInput=Setup.querySelector('input[name=fireball-color]');
-
+var Form=document.querySelector('.setup-wizard-form');
 
 
 var randomf=function(LengthArray){
@@ -33,8 +33,8 @@ var randomf=function(LengthArray){
     return RandomI;
 
   };
-
-var Wizards=[];
+//Ручное задание волшебников
+/*var Wizards=[];
 
 for (j=0; j<4; j++) {
   Wizards[j]={
@@ -42,23 +42,54 @@ for (j=0; j<4; j++) {
     coatColor: coatColors[randomf(coatColors.length)],
     eyesColor: eyesColors[randomf(eyesColors.length)]
     };
-}
-
+} */
+//Создание волшебника по шаблону
 var renderWizard = function (wizard){
   var element=template.cloneNode(true);
-    element.querySelector('.setup-similar-label').textContent=Wizards[i].name;
-    element.querySelector('.wizard-coat').style.fill=Wizards[i].coatColor;
-    element.querySelector('.wizard-eyes').style.fill=Wizards[i].eyesColor;
+    element.querySelector('.setup-similar-label').textContent=wizard.name;
+    element.querySelector('.wizard-coat').style.fill=wizard.colorCoat;
+    element.querySelector('.wizard-eyes').style.fill=wizard.colorEyes;
   return element;
 };
+// Вставка волшебников в случае успешного соединения с сервером
+var successHandler=function(wizards){
+  var fragment=document.createDocumentFragment();
+  var n=randomf(wizards.length);
+  //console.log(n+ ' '+wizards.length);
+  // Чтнение 4х волшебников из 17ти с сервера. Несколько тупое чтение, читает
+  // подряд 4-х начиная с рандомного числа массива волшебников, но чтобы избежать
+  // чтения Null читает начиная не более чем с 14ого волшебника.
+  if (n>=14){n=0}
+  for (var i=n; i<n+4; i++){
 
-var fragment=document.createDocumentFragment();
-for( var i=0; i<Wizards.length;i++){
- fragment.appendChild(renderWizard(Wizards[i]));
-  }
+    fragment.appendChild(renderWizard(wizards[i]));
+   }
 
-similarListElement.appendChild(fragment);
-document.querySelector('.setup-similar').classList.remove('hidden');
+  similarListElement.appendChild(fragment);
+  document.querySelector('.setup-similar').classList.remove('hidden');
+};
+//Неудачное соединение с сервером
+var errorHandler=function(errorMessage){
+     var node = document.createElement('div');
+    node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
+    node.style.position = 'absolute';
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = '30px';
+    node.classList.add('error-message');
+    node.textContent = errorMessage;
+    document.body.insertAdjacentElement('afterbegin', node);
+    CloseErrorMessage();
+
+  };
+  //Закрытие окна ошибки по клику
+  var CloseErrorMessage=function(){
+    var ErrorM=document.querySelector('.error-message');
+    //console.log(ErrorM);
+    ErrorM.addEventListener('click',function(){ErrorM.remove(); });
+
+
+  };
 
 //Вторая часть
 var onPopupEscPress=function(evt){
@@ -67,6 +98,7 @@ var onPopupEscPress=function(evt){
 
 var PopupOpen=function(){
 Setup.classList.remove('hidden');
+//window.backend.load(successHandler, errorHandler);
 document.addEventListener('keydown',onPopupEscPress);
 };
 
@@ -75,7 +107,9 @@ var PopupClose=function(){
   document.removeEventListener('keydown',onPopupEscPress);
   Setup.style.top=80+'px';
   Setup.style.left=50+'%';
+ // similarListElement.removeChild();
 };
+
 
 //Обработчик событие открытие окна установки персонажа
 SetupOpen.addEventListener('click', function(){
@@ -123,6 +157,12 @@ WizardEyes.addEventListener('click',function(){
 });
 FireballColor.addEventListener('click',function(){
   Changecolor3();
+});
+window.backend.load(successHandler, errorHandler);
+//Сохранение данных формы на сервер
+Form.addEventListener('submit',function(evt){
+  window.backend.save(new FormData(Form), function(responce){Setup.classList.add('hidden');},errorHandler );
+  evt.preventDefault();
 });
 
 })();
